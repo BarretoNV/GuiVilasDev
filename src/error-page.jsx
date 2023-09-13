15566c792277a605ac9d648fcde22103b9374d71
Guilherme Barreto <48062197+BarretoNV@users.noreply.github.com>
2023-09-12 23:02:44 -0300

    fix: padding in error page

--- src/error-page.jsx
+++ src/error-page.jsx
@@ -1,16 +1,19 @@
 import { useRouteError } from "react-router-dom";
+import { Container } from "react-bootstrap";
 
 export default function ErrorPage() {
   const error = useRouteError();
   console.error(error);
 
   return (
-    <div id="error-page" className="text-light">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
+    <Container>
+      <div id="error-page" className="text-light">
+        <h1>Oops!</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+        <p>
+          <i>{error.statusText || error.message}</i>
+        </p>
+      </div>
+    </Container>
   );
 }
